e81e096cf69239b24a3b9046a9783f88e1b3f862
karajrish <rishabh.karajgi@gmail.com>
2015-11-22 11:12:32 +0530

    pushed week3

---
+++ Week3/Ghost/app/src/main/java/com/google/engedu/ghost/GhostActivity.java
@@ -0,0 +1,104 @@
+package com.google.engedu.ghost;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Random;
+
+
+public class GhostActivity extends ActionBarActivity {
+    private static final String COMPUTER_TURN = "Computer's turn";
+    private static final String USER_TURN = "Your turn";
+    private GhostDictionary dictionary;
+    private boolean userTurn = false;
+    private Random random = new Random();
+    TextView wordfrag = (TextView) findViewById();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ghost);
+        InputStream is=null;
+        onStart(null);
+        try {
+            is = getAssets().open("words.txt");
+            SimpleDictionary sd = new SimpleDictionary(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_ghost, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+
+        String keyPressed = String.valueOf(event.getKeyCode());
+        if(isNumeric(keyPressed)){
+            return super.onKeyUp(keyCode, event);
+        }
+        else{
+
+        }
+
+    }
+    public static boolean isNumeric(String str) {
+        return str.matches("-?\\d+(\\.\\d+)?");  //match a number with optional '-' and decimal.
+    }
+
+    private void computerTurn() {
+        TextView label = (TextView) findViewById(R.id.gameStatus);
+        // Do computer turn stuff then make it the user's turn again
+        userTurn = true;
+        label.setText(USER_TURN);
+    }
+
+    /**
+     * Handler for the "Reset" button.
+     * Randomly determines whether the game starts with a user turn or a computer turn.
+     * @param view
+     * @return true
+     */
+    public boolean onStart(View view) {
+        userTurn = random.nextBoolean();
+        TextView text = (TextView) findViewById(R.id.ghostText);
+        text.setText("");
+        TextView label = (TextView) findViewById(R.id.gameStatus);
+        if (userTurn) {
+            label.setText(USER_TURN);
+        } else {
+            label.setText(COMPUTER_TURN);
+            computerTurn();
+        }
+        return true;
+    }
+}
